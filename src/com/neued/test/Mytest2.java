ded605d199112935fa7e5cecd81757daac7b08ba
庞文佳 <PWJ425520@163.com>
2020-02-07 15:24:29 +0800

    提交11

--- src/com/neued/test/Mytest2.java
+++ src/com/neued/test/Mytest2.java
@@ -1,4 +1,8 @@
 package com.neued.test;
 
 public class Mytest2 {
+
+    public static void main(String[] args) {
+        int a=0;
+    }
 }
--- src/com/neued/test/mytest.java
+++ src/com/neued/test/mytest.java
@@ -1,7 +1,26 @@
 package com.neued.test;
 
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class mytest {
     public static void main(String[] args) {
-
+        List arrayList=new ArrayList();
+        List linkedList=new LinkedList();
+        Date startarray=new Date();
+     for (int i=0;i<=1000000;i++){
+         arrayList.add(i);
+     }
+        Date endarray=new Date();
+        System.out.println("ArrayList添加用时="+(endarray.getTime()-startarray.getTime()));
+        Date startlinked=new Date();
+        for (int i=0;i<=1000000;i++){
+           linkedList.add(i);
+        }
+        Date endlinked=new Date();
+        System.out.println("LinkedList添加用时="+(endlinked.getTime()-startlinked.getTime()));
     }
 }
